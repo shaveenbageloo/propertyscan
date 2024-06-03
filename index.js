2a84bc50f0e1fcaa658725b53981eb46230d97be
Shaveen Bageloo <shaveen.bageloo@thewebduke.com>
2024-06-03 19:05:10 +0200

    Getting the basics right

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express")
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
 var app = express()
 app.get("/",function(request,response){
 response.send("Hello World!")
